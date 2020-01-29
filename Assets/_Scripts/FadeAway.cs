d4c43759f25e8d156c10d690bfc133df2db04800
ShaunFerns <shaun.ferns@tudublin.ie>
2020-01-29 09:28:14 +0000

    1st commit

---
+++ Assets/_Scripts/FadeAway.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(CountdownTimer))]
+public class FadeAway : MonoBehaviour
+{
+    private CountdownTimer countdownTimer;
+    private Text textUI;
+
+    void Awake()
+    {
+        textUI = GetComponent<Text>();
+        countdownTimer = GetComponent<CountdownTimer>();
+    }
+
+    void Start()
+    {
+        countdownTimer.ResetTimer(5);
+    }
+
+    void Update()
+    {
+        float alphaRemaining =
+        countdownTimer.GetProportionTimeRemaining();
+        print(alphaRemaining);
+        Color c = textUI.color;
+        c.a = alphaRemaining;
+        textUI.color = c;
+    }
+}
